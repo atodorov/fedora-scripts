7d840c2d995f95969615d39716b82ebc5f42b449
Alexander Todorov <atodorov@redhat.com>
2014-02-24 13:11:45 +0200

    add more test locations and update copyright

--- grep-for-tests.sh
+++ grep-for-tests.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 #
-#   Copyright (c) 2013, Alexander Todorov <atodorov@redhat.com>
+#   Copyright (c) 2013-2014, Alexander Todorov <atodorov@redhat.com>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ for f in `find $SEARCH_IN -name "*.src.rpm"`; do
     # extract the sources
     ./tools/extract-srpm $f ~/rpmbuild/BUILD >/dev/null 2>&1
 
-    find ~/rpmbuild/BUILD/ -type f | egrep "test/|tests/";
+    find ~/rpmbuild/BUILD/ -type f | egrep "SelfTest/|test/|tests/|t/|testing/|test_utils/|tests_utils/|testsuite/|tests.py|test.py|test_regex.py"
     if [ $? == 0 ]; then # grep matched something
         echo "^^^^^ $f"
     fi
